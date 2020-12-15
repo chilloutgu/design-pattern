8cfcc0a555c8e8fcbf99c7daccc30fb6f4411f2b
chillgu <bbonggu92@gmail.com>
2020-12-15 23:06:09 +0900

    add a new sungard class instance & set strategy with lambda

--- strategy/Client.java
+++ strategy/Client.java
@@ -10,6 +10,10 @@ public class Client {
     taekwonV.setAttackBehavior(new MissileBehavior());
     taekwonV.setMoveBehavior(new WalkingBehavior());
 
+    Robot sungard = new Sungard("sunguard");
+    sungard.setAttackBehavior(() -> System.out.println("레이저 공격!"));
+    sungard.setMoveBehavior(() -> System.out.println("순간 이동!"));
+
     System.out.println("My name is " + atom.getName());
     atom.performMove();
     atom.performAttack();
@@ -19,5 +23,10 @@ public class Client {
     System.out.println("My name is " + taekwonV.getName());
     taekwonV.performMove();
     taekwonV.performAttack();
+
+    System.out.println();
+    System.out.println("My name is " + sungard.getName());
+    sungard.performMove();
+    sungard.performAttack();
   }
 }
