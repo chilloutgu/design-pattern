bee2d0d2aef4405c6f4972a6b0d4884d9e8c349f
chillgu <bbonggu92@gmail.com>
2020-12-15 22:42:36 +0900

    update method & add new class

--- strategy/Atom.java
+++ strategy/Atom.java
@@ -13,6 +13,7 @@ public class Atom extends Robot {
 
   @Override
   public void move() {
-    System.out.println("날기!");
+    // TaekonV 의 move 매서드와 중복됨과 동시에 새로운 기능으로 변경하기 위해 기존의 코드를 변경하고 있으므로 OCP 위반.
+    System.out.println("걷기!");
   }
 }
---
+++ strategy/Sungard.java
@@ -0,0 +1,18 @@
+package strategy;
+
+public class Sungard extends Robot {
+  public Sungard(String name) {
+    super(name);
+  }
+
+  @Override
+  public void attack() {
+    // TaekonV 의 attack 메서드와 중복됨.
+    System.out.println("미사일 공격!");
+  }
+
+  @Override
+  public void move() {
+    System.out.println("날기!");
+  }
+}
