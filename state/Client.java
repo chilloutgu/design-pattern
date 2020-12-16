8d4dc9465815e53e7b0ea295adb86a164165e621
chillgu <bbonggu92@gmail.com>
2020-12-16 10:31:27 +0900

    add sleeping mode

--- state/Client.java
+++ state/Client.java
@@ -3,9 +3,11 @@ package state;
 public class Client {
   public static void main(String[] args) {
     Light light = new Light();
-    light.on();
-    light.on();
-    light.off();
-    light.off();
+    light.on(); /* Light on! */
+    light.on(); /* Sleeping mode */
+    light.on(); /* Light on! */
+    light.on(); /* Sleeping mode */
+    light.off(); /* Light off! */
+    light.off(); /* 반응 없음 */
   }
 }
--- state/Light.java
+++ state/Light.java
@@ -1,28 +1,36 @@
 package state;
 
 public class Light {
-  private static final int ON = 1;
   private static final int OFF = 0;
+  private static final int ON = 1;
+  private static final int SLEEPING = 2;
 
-  private int state;
+  private int currentState;
 
   public Light() {
-    this.state = OFF;
+    this.currentState = OFF;
   }
 
   public void on() {
-    if (this.state == OFF) {
+    if (this.currentState == OFF) {
       System.out.println("Light on!");
-      this.state = ON;
+      this.currentState = ON;
+    } else if (this.currentState == SLEEPING) {
+      System.out.println("Light on!");
+      this.currentState = ON;
     } else {
-      System.out.println("반응 없음");
+      System.out.println("Sleeping mode!");
+      this.currentState = SLEEPING;
     }
   }
 
   public void off() {
-    if (this.state == ON) {
+    if (this.currentState == ON) {
       System.out.println("Light off!");
-      this.state = OFF;
+      this.currentState = OFF;
+    } else if (this.currentState == SLEEPING) {
+      System.out.println("Light off!");
+      this.currentState = OFF;
     } else {
       System.out.println("반응 없음");
     }
